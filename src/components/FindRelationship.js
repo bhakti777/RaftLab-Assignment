5539f272e2cd890abe8e81c59b7f419728a31b1a
bhakti777 <bhakti1605@gmail.com>
2022-04-17 00:25:53 +0530

    handling connections

--- src/components/FindRelationship.js
+++ src/components/FindRelationship.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import RelationShipDropdown from "./RelationShipDropdown";
 
-const FindRelationship=()=>{
+const FindRelationship = ({ users, relationships }) => {
 
-    return(
+    const [displayRelationShip, setDisplayRelationShip] = useState([])
+
+
+    const handleFindRelationShip = (formState) => {
+        const firstPersonFriends = relationships[formState.person1];
+        const secondPersonFriends = relationships[formState.person2];
+        const filteredArray = firstPersonFriends.filter(value => secondPersonFriends.includes(value));
+        console.log("Common Frnz =>", filteredArray)
+        setDisplayRelationShip([...displayRelationShip, [formState.person1, filteredArray.join(" > "), formState.person2]])
+    }
+
+    return (
         <>
+            <h1>Find Relationships</h1>
+            <RelationShipDropdown users={users} handleOnClick={handleFindRelationShip} buttonName={"Find"} />
+            {displayRelationShip && displayRelationShip.map((connections) => {
+                return connections.join(">")
+            })}
         </>
     )
 }
